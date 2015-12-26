70388ae70c51e3f4c88636238ba7acca378b60a2
Sören Grzanna <soeren.grzanna@stud.fh-hannover.de>
2015-12-26 13:08:14 +0100

    Version 1.06

--- OGameSpyReportSorter.user.js
+++ OGameSpyReportSorter.user.js
@@ -2,7 +2,7 @@
 // @name        OGame spy report sorter
 // @namespace   ogame
 // @include     http://*.ogame.gameforge.com/game/index.php?page=messages*
-// @version     1.05
+// @version     1.06
 // @updateURL   https://github.com/Doggi/OGameSpyReportSorter/raw/master/OGameSpyReportSorter.user.js
 // @downloadURL https://github.com/Doggi/OGameSpyReportSorter/raw/master/OGameSpyReportSorter.user.js
 // @grant       none
@@ -181,7 +181,7 @@ var grosseTransporterAM = 203;
                 },
                 wertigkeit: function () {
                     endWhile = false;
-                    preBeute = this.beute();
+                    preBeute = this.rohstoffe;
                     pb = 0;
                     for (i = 1; pb !== null; i++) {
                         pb = getStorageItem("spioreports_clicks_" + this.msgId + "_" + i + "_" + this.beute());
